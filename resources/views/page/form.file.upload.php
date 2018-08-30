21e6c88bef0c0cfe9fa6c03cde9b18bfc8971dec
Kitsune Solar <kitsune.solar@gmail.com>
2018-08-30 18:28:13 +0300

    2018-08-30 15:28:13

--- resources/views/page/form.file.upload.php
+++ resources/views/page/form.file.upload.php
@@ -1,4 +1,4 @@
-<?php use MetaStore\App\Kernel\View; ?>
+<?php use MetaStore\App\{Kernel\View, Cloud\Config}; ?>
 
 <?php View::get( 'header', '_common' ); ?>
 
@@ -26,7 +26,7 @@
 					<div class="field">
 						<div class="control has-icons-left">
 							<div class="file">
-								<input id="getFile" name="getFile" class="ext-reset" type="file" accept="" required />
+								<input id="getFile" name="getFile" class="ext-reset" type="file" accept="<?php echo implode( ',', Config\Upload::getMime( 'allow' ) ) ?>" required />
 							</div>
 						</div>
 						<p class="help has-text-grey">
